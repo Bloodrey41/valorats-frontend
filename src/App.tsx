0687893d6875f76b193018dd63897c5308156ade
Miguel Angel Robledo Gutiérrez <desarrollo@escueladidactica.com>
2022-12-02 19:46:20 -0500

    feat: :sparkles: Disabled refetchOnWindowFocus

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Main from './pages/main';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    defaultOptions: {
+	queries: {
+	    refetchOnWindowFocus: false
+	}
+    }
+})
 
 const App = () => {
     return (
